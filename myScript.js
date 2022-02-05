1303bf31a358ba3f8b39886cd192d6db6fb57ef3
imousa54 <ibryou@hotmail.com>
2022-02-05 14:28:07 +0400

    Update myScript.js
    update URL for documents report references

--- myScript.js
+++ myScript.js
@@ -1,3 +1,15 @@
+function checkURLOnPageLoad() {
+	let queryString = window.location.search;
+	let urlParams = new URLSearchParams(queryString);
+	let part = urlParams.get('part');
+	let partNumber = Number(part);
+	if (part && partNumber && partNumber >= 1 && partNumber <= 16 & partNumber != 11) {
+		selectPart(document.getElementById('part'+part), part);
+	}
+}
+
+window.onload = checkURLOnPageLoad;
+
 function selectPart(source, n) {
 	changeListStyle(source);
 	document.getElementById('partsdata').src = "parts/part_"+n+"/index.html";
