5c033ae3a4e663aca5a8242c60d1f80064f47e5d
imousa54 <ibryou@hotmail.com>
2022-02-22 01:07:31 +0400

    Major release for part 2
    Major release for part 2

---
+++ lab2/myScript.js
@@ -0,0 +1,24 @@
+function checkURLOnPageLoad() {
+	let queryString = window.location.search;
+	let urlParams = new URLSearchParams(queryString);
+	let part = urlParams.get('part');
+	let partNumber = Number(part);
+	if (part && partNumber && partNumber >= 0 && partNumber <= 7) {
+		selectPart(document.getElementById('part'+part), part);
+	}
+}
+
+window.onload = checkURLOnPageLoad;
+
+function selectPart(source, n) {
+	changeListStyle(source);
+	document.getElementById('partsdata').src = "parts/part_"+n+"/index.html";
+}
+
+function changeListStyle(source) {
+	var x = document.getElementById("menulist").getElementsByTagName("li");
+	for (let i = 0; i < x.length; i++) {
+	  x[i].className = 'menuitem';
+	}
+	source.className = 'selectedmenuitem';
+}
